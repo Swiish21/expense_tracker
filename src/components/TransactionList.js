11b3543a3de786a8bc37c15a23807e7905ddcb21
Malcom Otieno <malcolmotieno16@gmail.com>
2024-12-09 23:20:14 +0300

    minor changes to the transaction list component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
-const TransactionList = () => {
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * Component that renders a list of transactions. It also renders a summary of
+ * income and expense.
+ * @return {ReactElement} The TransactionList component.
+ */
+/******  a312dfd5-a9eb-4d90-92dc-bbae7350f0e1  *******/
+export const TransactionList = () => {
+
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <div>
             <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
+        {transactions.map(transaction => (
+          <li key={transaction.id}>
+            <span>{transaction.text}</span>
+            <span className={transaction.amount > 0 ? 'plus' : 'minus'}>
+              {transaction.amount}
+            </span>
+            <button className="delete-btn">x</button>
+          </li>
+        ))}
+        <li className="inc-exp-container">
+          <div>
+            <h4>Income</h4>
+            <p className="money plus">+$0.00</p>
+          </div>
+          <div>
+            <h4>Expense</h4>
+            <p className="money minus">-$0.00</p>
+          </div>
         </li>
       </ul>
     </div>
